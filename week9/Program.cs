3b0748d64ff2a5b861dc2fa5a596029162ecd1ae
Jesse Viau <jesse.viau92@stclairconnect.ca>
2021-03-30 22:47:36 -0400

    Week 9

---
+++ week9/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace week9{
+
+    class Program{
+
+        static void Main(string[] args){
+            var setOfDie2 = Dice.GroupBy(die => die.Value)
+                                .Select(die => {
+                return new {
+                    Value = die.Key,
+                    Count = die.Count(),
+                    Score = die.Key * die.Count()
+                };
+            }).OrderBy(Die => Die.Value);
+            
+        }
+
+
+
+    }
+
+
+
+
+
+}
